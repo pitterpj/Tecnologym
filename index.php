8233fd49ebba4ca0f318d37ebf33413444bc14b4
Pitter <pedrojc@outlook.es>
2023-02-07 19:40:09 +0100

    config.php completed!

---
+++ index.php
@@ -0,0 +1,15 @@
+<!-- Pedro.J (Pitter) -->
+<!-- https://github.com/pitterpj -->
+
+<!-- 
+    How the Model-View-Controller works:
+        URI: http://domain.com/subfolders/controller/method/param1/param2/param3...
+        -> Domain: domain where the MVC is
+        -> Folders and subfolders: where resource is
+        -> Controller: Controller where methods are
+        -> Method: Methods with the parameters
+        ->Parameter: Parameters used  
+-->
+<?php
+ echo $_SERVER['DOCUMENT_ROOT'] 
+?>
---
+++ system/config.php
@@ -0,0 +1,25 @@
+<!-- Pedro.J (Pitter) -->
+<!-- https://github.com/pitterpj -->
+
+
+<?php
+//  **** GENERAL CONFIGURATION  **** //
+define("URI", $_SERVER['REQUEST_URI']); // Return the URI of th current page
+define("DEFAULT_CONTROLLER", "Home_c"); // This is the default controller
+define("DEFAULT_METHOD", "Index"); // This is the default method
+define("CORE", "system/core"); // Path to the core
+define("PATH_CONTROLLERS", "app/controllers/"); // Path to the controllers
+define("PATH_VIEWS", "app/views/"); // Path to the views
+define("PATH_MODELS", "app/models/"); // Path to the models
+
+// **** Change this setting for each project **** //
+define("ROOT", $_SERVER['DOCUMENT_ROOT'] . '/MVC_Template/');
+define("BASE_URL", "http://localhost/MVC_Template");
+
+// **** DATABASE **** //
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASS","");
+define("DB_NAME","MVC_Template"); // Change this!
+
+?>
