e0883c221008254407288279c74351e6a2344b37
Pitter <pedrojc@outlook.es>
2023-02-08 12:10:51 +0100

    Complete Model.php

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ## Author: Pedro.J (Pitter)
 ### https://github.com/pitterpj
 
+
+La función consultar del Model (consult) no me convence el nombre
+Ejecutar = launch()
--- system/core/Model.php
+++ system/core/Model.php
@@ -17,7 +17,70 @@ class Model
 
     function __construct()
     {
-        
+        // Create DSN
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+        );
+
+        // Test connection
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch (PDOException $e) {
+            // Capture all PDOExcepction errors
+            $this->error = $e->getMessage();
+        }
+    } //End construct
+
+    //****  Methods ****//
+    public function consult($query)
+    {
+        $this->stmt = $this->dbh->prepare($query);
+    }
+
+    public function link($param, $value, $type = null)
+    {
+        // Performs the bindValue and sets the type of params
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function launch()
+    {
+        // Return a result object with all selected rows.
+        return $this->stmt->execute();
+    }
+
+    public function resultado()
+    { // Returns the results in an associative array
+        // Calls the launch function to execute the query
+        $this->launch();
+
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function row()
+    { // Returns a single result
+        // Calls the launch function to execute the query
+        $this->launch();
+
+        return $this->stmt->fech(PDO::FETCH_ASSOC);
     }
 } //End class Model
 ?>
