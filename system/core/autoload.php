974e1c542bb84e54a5f12d84efb278fed70592d2
Pitter <pedrojc@outlook.es>
2023-02-09 14:10:52 +0100

    Config config.php

--- system/core/autoload.php
+++ system/core/autoload.php
@@ -7,9 +7,7 @@
 /* This function (anonymous) loads all classes in the file where it is included.
 In this case in Index*/
 spl_autoload_register(function ($className) {
-    if (is_file(CORE . $className . ".php")) {
-        include CORE . $className . ".php";
-    }
+    if (is_file(CORE . $className . ".php")) include CORE . $className . ".php";
 });
 
 
