09f3d42851abbca000901d943a5bc3a2857f305c
Pitter PJ <pedrojc@outlook.es>
2023-04-17 10:36:35 +0200

    format hour

--- app/views/director/v_Director.php
+++ app/views/director/v_Director.php
@@ -6,21 +6,21 @@
 <div class="row container-fluid justify-content-around">
     <div class="container col-lg-5 col-md-9 col-sm-8 fondo-2 shadow p-3 mb-5 rounded">
         <div class="card h-100">
-            <div class="card-header text-center">Horario personal (D)</div>
+            <!-- <div class="card-header text-center mr-5">CLASES DIRIGIDAS:</div> -->
             <div class="card-body">
-                <h4 class="card-title text-center">Hoy</h4>
-                <table class="table table-primary table-striped table-hover">
+                <h4 class="card-title text-center ">HOY</h4>
+                <table class="table table-hover">
                     <thead>
                         <tr class="text-center">
-                            <th scope="col">Imagen</th>
-                            <th scope="col">Hora</th>
+                            <th scope="col">CLASE</th>
+                            <th scope="col">HORA</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php foreach ($personalScheduleToday as $class) : ?>
                             <tr class="text-center">
                                 <td scope="row"><img width="100" src="<?= BASE_URL . IMG_URL . $class['img'] ?>"></td>
-                                <td scope="row"><?= $class['hour'] ?></td>
+                                <td scope="row"><?= substr($class['hour'], 0, 5); ?></td>
                             </tr>
                         <?php endforeach ?>
                     </tbody>
@@ -29,17 +29,21 @@
         </div>
     </div>
     <!--==== col 2 ====-->
-    <div class="container col-lg-5 col-md-9 col-sm-8 ">
+    <div class="container col-lg-5 col-md-9 col-sm-8">
         <!--==== row 2 ====-->
         <div class="row fondo-2 shadow p-3 mb-5 rounded">
             <!--==== col2.1 ====-->
             <div class="col-12 card">
-                <div class="card-header text-center">Entrenamientos personales esta semana</div>
+                <!-- <div class="card-header text-center">ENTRENOS PERSONALES:</div> -->
                 <div class="card-body text-center">
-                    <h4 class="card-title text-center">Entrenos</h4>
+                    <h4 class="card-title text-center">ENTRENOS PARA HOY</h4>
                     <p class="card-text">
                     <h3> <?= $logTraining['log'] ?> </h3>
                     </p>
+                    <div class="d-flex justify-content-around ">
+                        <a name="addClient" class="btn btn-color" href="<?= BASE_URL . "c_PersonalTraining/addPersonalTraining" ?>" role="button">AÑADIR</a>
+                        <a name="addClient" class="btn btn-color" href="<?= BASE_URL . "c_PersonalTraining/showPersonalTraining" ?>" role="button">VER TODOS</a>
+                    </div>
                 </div>
             </div>
         </div> <!--==== end row2 ====-->
@@ -47,7 +51,7 @@
         <div class="row fondo-2 shadow p-3 mb-5 rounded">
             <!--==== col 2.2 ====-->
             <div class="col-12 card ">
-                <div class="card-header text-center">Notificaciones</div>
+                <!-- <div class="card-header text-center">Notificaciones</div> -->
                 <div class="card-body text-center">
                     <h4 class="card-title text-center">Notificaciones</h4>
                     <p class="card-text">No tiene notificaciones actualmente</p>
